fa644b43584cdbf284c6877c360e0f1428a76c81
sushi057 <champsuvash@gmail.com>
2022-10-17 22:06:44 +0545

    Logo header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+const header = document.createElement('header');
 
 const content = document.querySelector('#content');
 
-const header = document.createElement('header');
 
+//Left Section with logo
+const logoSection = document.createElement('div');
+
+const logoImage = document.createElement('img');
+logoImage.src = "Restaurant Page/src/images/logo.jpg";
+logoImage.alt = 'npp logo';
+logoImage.classList.add('logo-img');
+logoSection.appendChild(logoImage);
+
+const logoText = document.createElement('h2');
+logoText.textContent = 'NPP BURGER HOUSE';
+logoText.classList.add('logo-text');
+logoSection.appendChild(logoText);
 
