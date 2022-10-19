8fc01ce4b0d9178a5dfb80c61ea64ae6c7a74423
sushi057 <champsuvash@gmail.com>
2022-10-19 20:15:22 +0545

    Bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,28 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    home: './src/home.js',
+    menu: './src/menu.js',
+    gallery: './src/gallery.js',
+    contacct: './src/contact.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Page',
+    }),
+  ],
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
